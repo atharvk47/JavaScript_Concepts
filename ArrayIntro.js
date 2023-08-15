b4b43416990fcfe15c2383baf9b2ea646bca50fc
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-08-15 23:16:33 +0530

    'Array' file added.

---
+++ ArrayIntro.js
@@ -0,0 +1,22 @@
+// An array can hold many values under a single name, and you can access the values by referring to an index number.
+
+const cars = ["Saab", "Volvo", "BMW"];
+
+// We can declare an array and then initialize the values within it.
+
+names = [];
+
+
+// The elements of an array can be accessed using an index number.
+
+names[0] = "Batman";
+names[1] = "Superman";
+names[2] = "Ironman";
+names[3] = "Spiderman";
+
+console.log(names[2]);
+console.log(cars[1]);
+
+// Output:
+// Ironman
+// Volvo
