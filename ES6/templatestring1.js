bd72e78bb759f50218f6dff24b70e6bb90021855
ThisisAtharv <atharv.kurlapkar47@gmail.com>
2023-08-19 22:31:56 +0530

    'Template String' file added.

---
+++ ES6/templatestring1.js
@@ -0,0 +1,12 @@
+
+// Template Literals use back-ticks (``) rather than the quotes ("") to define a string.
+
+let text = `Hello World!`;
+console.log(text);
+
+// With template literals, you can use both single and double quotes inside a String. 
+
+let subtext = `He's often called "Johnny"`;
+console.log(subtext);
+
+// Template literal can be used to escape single and double quotes.
